24bb2c9a4a9b78e70c586febe5683a06c9e48fa8
donghyun kim <imkim1893@naver.com>
2022-05-26 01:12:21 +0900

    HiveUtil init

--- lib/_common/util/hive/HiveUtil.dart
+++ lib/_common/util/hive/HiveUtil.dart
@@ -1,8 +1,8 @@
-
 import 'package:crawller_backend/_common/util/firebase/firedart/FiredartAuthSingleton.dart';
 import 'package:hive/hive.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+
 class HiveUtil {
   static Future<void> init() async {
     var appDir = await getApplicationDocumentsDirectory();
