23cc4e95b3e05d3e7c5931ba2f6e0cf715eae349
donghyun kim <imkim1893@naver.com>
2022-05-26 01:19:09 +0900

    불필요한 라이브러리 제거

--- lib/_common/util/hive/HiveUtil.dart
+++ lib/_common/util/hive/HiveUtil.dart
@@ -1,12 +1,11 @@
+import 'dart:io';
+
 import 'package:crawller_backend/_common/util/firebase/firedart/FiredartAuthSingleton.dart';
 import 'package:hive/hive.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 
 class HiveUtil {
   static Future<void> init() async {
-    var appDir = await getApplicationDocumentsDirectory();
-    Hive.init(join(appDir.path, null));
+    Hive.init(Directory.current.absolute.path);
     Hive.registerAdapter(TokenAdapter());
   }
 }
