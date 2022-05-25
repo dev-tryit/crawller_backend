cbbf912fecbbb0d5db64d7335b1523d5a06ce003
KimDonghyun <imkim189371@gmail.com>
2022-05-25 12:39:18 +0900

    admin 계정을 통해, firestore에 접근하기로 정책 결정함

--- lib/MySetting.dart
+++ lib/MySetting.dart
@@ -1,6 +1,5 @@
 class MySetting {
   static const bool isRelease = false;
-
-  // App
   static const String appName = "function_backend";
+  static const String serverAddress = "https://sumgo-crawller-backend-x5bi7ohlhq-du.a.run.app/";
 }
